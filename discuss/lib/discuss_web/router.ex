6acc8d5d8842bdedb6355a1ad250aeab6d2b11ec
Kurund Jalmi <kurund.jalmi@gmail.com>
2022-02-05 22:28:00 +0000

    more work

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -18,6 +18,8 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    get "topics/new", TopicController, :new
   end
 
   # Other scopes may use custom stacks.
